b0226fd0eb91c767f928f142373047830857747f
edutello <huedenot@gmail.com>
2025-10-20 23:59:31 -0500

    Este es mi tercer commit

--- hellogit.py
+++ hellogit.py
@@ -1,1 +1,18 @@
+import random
 print("Hello Git")
+
+numero_secreto = random.randint(1, 10)
+intento = 0
+
+print("Adivina el número del 1 al 10")
+
+while True:
+    intento = int(input("Tu intento: "))
+
+    if intento == numero_secreto:
+        print("🎉 ¡Adivinaste! El número era", numero_secreto)
+        break
+    elif intento < numero_secreto:
+        print("Demasiado bajo 😅")
+    else:
+        print("Demasiado alto 😅")
